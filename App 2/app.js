40c35f50df7e9acd69dbb8f70f02ec331509bc63
TestUser <rodrigo_luizp@hotmail.com>
2019-07-03 20:52:01 -0300

    error dec

--- App 2/app.js
+++ App 2/app.js
@@ -1,13 +1,49 @@
 const slider = document.getElementById("formControlRange");
 const output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
+let time = 1;
 
 slider.oninput = function() {
   output.innerHTML = this.value;
   const amount = document.getElementById("amount");
   const slider = document.getElementById("formControlRange");
   const resultado = document.getElementById("result");
-
+  
+  if(amount.value > 0)
   resultado.value = Math.ceil(parseFloat(amount.value) * 12000 / slider.value);
+  else
+  {
+    showError('Por favor, verifique o valor de entrada',time);
+
+  }
+
+
 };
 
+function showError(error,times){
+
+time = times;
+
+if(time >0)
+{
+const card = document.querySelector('.card');
+const heading = document.querySelector('.heading');
+
+
+  const errorDiv = document.createElement('div');
+
+  errorDiv.className = 'alert alert-danger';
+
+  errorDiv.appendChild(document.createTextNode(error));
+
+  card.insertBefore(errorDiv, heading);
+
+  setTimeout(clearError, 3000);
+  time = time - 1;
+}
+}
+
+function clearError(){
+  document.querySelector('.alert ').remove();
+  time = time + 1;
+}
